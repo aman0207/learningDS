bf6b4f373ccb6555db22953596cc8d053124d145
aman0207 <aman27verma.mohit@gmail.com>
2022-06-06 22:16:57 +0530

    array soer012

---
+++ src/ArraySort012.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class ArraySort012 {
+    public static void main(String[] args) {
+        int[] arr = {0,2,1,2,0};
+        Arrays.sort(arr);
+        Utility.printArray(arr);
+        
+    }
+}
---
+++ src/Utility.java
@@ -0,0 +1,10 @@
+public class Utility {
+
+    public static void printArray(int[] arr){
+        for(int a : arr){
+            System.out.print(a+" ");
+        }
+        System.out.println();
+    }
+    
+}
